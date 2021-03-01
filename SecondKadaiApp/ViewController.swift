1addbcb568c78e353f334f6d05be1322aa45834c
takahiro.abe <t.abe.0802@gmail.com>
2021-03-01 02:04:05 -0600

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Takahiro Abe on 3/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
